3fab28586c3f6fc4529e76c5ea14fdf183c6a759
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-11-01 14:51:11 +0900

    array-trying

---
+++ array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" >
+	<title>配列を使ってみよう</title>
+	<link rel="stylesheet" href="/php/css/skyblue.css">
+</head>
+<body>
+	<div class="bg-success padding-y-20">
+		<div class="container text-center">
+			<h1>配列を使ってみよう</h1>
+		</div>
+	</div>
+	<div class="container padding-y-20">
+		<!-- ここから -->
+		<!-- ここまで -->
+	</div>
+</body>
+</html>
---
+++ array_comp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" >
+	<title>配列を使ってみよう</title>
+	<link rel="stylesheet" href="/php/css/skyblue.css">
+</head>
+<body>
+	<div class="bg-success padding-y-20">
+		<div class="container text-center">
+			<h1>配列を使ってみよう</h1>
+		</div>
+	</div>
+	<div class="container padding-y-20">
+		<?php 
+		$season = [ "春", "夏", "秋", "冬" ];
+		
+		echo $season[0];
+		?>
+	</div>
+</body>
+</html>
